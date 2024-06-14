aeea08d6965bdaf3759858e337040ce0659529bb
Ihor Chovpan <i.chovpan@gmail.com>
2024-06-14 19:45:34 -0400

    Change repo name in the codebase to shorturl

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/chopikus/url-shortener
+module github.com/chopikus/shorturl
 
 go 1.22.3
 
--- http-handlers.go
+++ http-handlers.go
@@ -6,7 +6,7 @@ import (
     "fmt"
     "log"
     "database/sql"
-    "github.com/chopikus/url-shortener/templates"
+    "github.com/chopikus/shorturl/templates"
     "encoding/json"
 )
 
