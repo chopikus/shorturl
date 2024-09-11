f943643dd14106a9be4e26fbdaea8f5322bca6d1
Ihor Chovpan <i.chovpan@gmail.com>
2024-09-11 19:16:26 -0400

    Sleep between 0 and 1 seconds between requests.

--- load-testing/api-test.js
+++ load-testing/api-test.js
@@ -1,6 +1,7 @@
 // import necessary modules
 import { check, sleep, group } from 'k6';
 import http from 'k6/http';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 // define configuration
 export const options = {
@@ -8,6 +9,7 @@ export const options = {
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(99)<1000'], // 99% of requests should be below 1s
+
   },
 };
 
@@ -19,6 +21,7 @@ function generateUrl() {
 
 export default function () {
   group('Create and get', function () {
+        sleep(Math.random());
         const params = {
           headers: {
               'Content-Type': 'application/json',
@@ -37,15 +40,16 @@ export default function () {
             'has urlCode': (r) => r.json().hasOwnProperty('urlCode'),
             'has expiresOn': (r) => r.json().hasOwnProperty('expiresOn'),
         });
-
+        
+        sleep(Math.random());
         const urlCode = res.json().urlCode;
-        sleep(5);
         
         /* Each link is shared to 10 different people who open a link within 10 seconds. */
         for (let i = 0; i < 10; i++) {
             const res = http.get('http://localhost:8000/' + urlCode);
             check(res, { 'status is 200': (r) => r.status === 200 });
-            sleep(1);
+
+            sleep(Math.random());
         }
   });
 }
