e04abfb96b2adb0577911d9b3c8c64136e36e04a
Ihor Chovpan <i.chovpan@gmail.com>
2024-09-11 14:52:05 -0400

    First implementation of api test k6 script.

---
+++ load-testing/api-test.js
@@ -0,0 +1,51 @@
+// import necessary modules
+import { check, sleep, group } from 'k6';
+import http from 'k6/http';
+
+// define configuration
+export const options = {
+  // define thresholds
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(99)<1000'], // 99% of requests should be below 1s
+  },
+};
+
+let counter = 0;
+function generateUrl() {
+    counter += 1;
+    return "https://example" + counter + ".lol";
+}
+
+export default function () {
+  group('Create and get', function () {
+        const params = {
+          headers: {
+              'Content-Type': 'application/json',
+          }
+        };
+        const payload = JSON.stringify({
+            urlOriginal: generateUrl()
+        });
+
+        const url = 'http://localhost:8000/api/new';
+        const res = http.post(url, payload, params);
+
+        check(res, {
+            'status is 200': (r) => r.status === 200,
+            'has urlOriginal': (r) => r.json().hasOwnProperty('urlOriginal'),
+            'has urlCode': (r) => r.json().hasOwnProperty('urlCode'),
+            'has expiresOn': (r) => r.json().hasOwnProperty('expiresOn'),
+        });
+
+        const urlCode = res.json().urlCode;
+        sleep(5);
+        
+        /* Each link is shared to 10 different people who open a link within 10 seconds. */
+        for (let i = 0; i < 10; i++) {
+            const res = http.get('http://localhost:8000/' + urlCode);
+            check(res, { 'status is 200': (r) => r.status === 200 });
+            sleep(1);
+        }
+  });
+}
